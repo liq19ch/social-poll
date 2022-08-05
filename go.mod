f0bb03b363fba1e3c8d24ac7246284701d6d7b06
FP-OD1133 <s403160087@gmail.com>
2022-08-05 13:51:14 +0800

    init

---
+++ go.mod
@@ -0,0 +1,19 @@
+module socialpoll
+
+go 1.18
+
+require (
+	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/nsqio/go-nsq v1.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ twittervotes/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+func main() {
+	var stoplock sync.Mutex
+	stop := false
+	stopChan := make(chan struct{}, 1)
+	signalChan := make(chan os.Signal, 1)
+	go func() {
+		<-signalChan
+		stoplock.Lock()
+		stop = true
+		stoplock.Unlock()
+		log.Println("Stopping...")
+		stopChan <- struct{}{}
+		closeConn()
+	}()
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	if err := dialdb(); err != nil {
+		log.Fatalln("failed to dial MongoDB:", err)
+	}
+	defer closedb()
+	votes := make(chan string)
+	publisherStoppedChan := publishVotes(votes)
+	twitterStoppedChan := startTwitterStream(stopChan, votes)
+	go func() {
+		// refresher
+		for {
+			time.Sleep(1 * time.Minute)
+			closeConn()
+			stoplock.Lock()
+			if stop {
+				stoplock.Unlock()
+				return
+			}
+			stoplock.Unlock()
+		}
+	}()
+
+	<-twitterStoppedChan
+	close(votes)
+	<-publisherStoppedChan
+}
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("dialing mongodb: localhost")
+	db, err = mgo.Dial("127.0.0.1:27017")
+	return err
+}
+
+func closedb() {
+	db.Close()
+	log.Println("closed database connection")
+}
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
